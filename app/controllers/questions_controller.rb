d4cbfe30381f7f68cab8a83972b04039ab210960
Jackie Mundrawala <jmundrawala@gmail.com>
2016-04-29 00:03:39 +0000

    Add new questions page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,4 +3,7 @@ class QuestionsController < ApplicationController
 	def index
 
 	end
+
+	def new
+	end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,10 +2,16 @@ require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
 	describe "questions#index action" do
-		it "show successfully show the index page" do 
+		it "should show successfully show the index page" do 
 			get :index
 			expect(response).to have_http_status(:success)
 		end 
 	end
 	
+	describe "questions#new action" do
+		it "should successfully show the new form" do
+			get :new
+			expect(response).to have_http_status(:success)
+		end
+	end
 end
