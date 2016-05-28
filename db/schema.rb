a00582f009be6f3f69436f0535480d7f11a0e7aa
Kaoru Kohashigawa <Kaoru@siftscience.com>
2016-05-28 15:12:10 -0700

    adds controllers and simple views to start allow user sign ups and allowing folks to add questions / companies

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20160420092853) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string   "content"
+    t.text     "content"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
